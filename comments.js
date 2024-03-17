1c60d6b009f66c3e9fb3fb7d62c772a9b8811371
Shivam Upadhayay <53188725+shiv159@users.noreply.github.com>
2024-03-17 15:00:06 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,36 +1,12 @@
+const http = require('http');
+
 // create web server
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var url = require('url');
-var port = process.env.PORT || 3000;
-
-var server = http.createServer(function(req, res){
-  var uri = url.parse(req.url).pathname;
-  var filename = path.join(process.cwd(), uri);
-  fs.exists(filename, function(exists){
-    if(!exists){
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.write('404 Not Found\n');
-      res.end();
-      return;
-    }
-    if(fs.statSync(filename).isDirectory()) filename += '/index.html';
-    fs.readFile(filename, 'binary', function(err, file){
-      if(err){
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.write(err + '\n');
-        res.end();
-        return;
-      }
-      res.writeHead(200);
-      res.write(file, 'binary');
-      res.end();
-    });
-  });
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, world!');
 });
 
-server.listen(parseInt(port, 10), function(){
-  console.log('Server listening on port ' + port);
+server.listen(3000, 'localhost', () => {
+  console.log('Server running at http://localhost:3000/');
 });
-
