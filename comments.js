2de82b05e4a70fb7e1e5ca6385c5675dd394a7e2
Shivam Upadhayay <53188725+shiv159@users.noreply.github.com>
2024-03-17 15:06:49 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,36 @@
-const http = require('http');
-
 // create web server
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, world!');
+// create api
+// create comments api
+// get
+// post
+// delete
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const comments = require('./comments');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/api/comments', (req, res) => {
+    res.json(comments.getComments());
 });
 
-server.listen(3000, 'localhost', () => {
-  console.log('Server running at http://localhost:3000/');
+app.post('/api/comments', (req, res) => {
+    const { name, comment } = req.body;
+    comments.addComment(name, comment);
+    res.json(comments.getComments());
+});
+
+app.delete('/api/comments/:id', (req, res) => {
+    const { id } = req.params;
+    comments.deleteComment(id);
+    res.json(comments.getComments());
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
 });
